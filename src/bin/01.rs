67f604c172da1498f9989bbf973fde89dad38072
Vivek Kalyan <hello@vivekkalyan.com>
2024-12-01 15:50:40 +0800

    Solve day 01 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -32,7 +34,34 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut right_list: HashMap<u32, u32> = HashMap::new();
+    let left_list: Vec<u32> = input
+        .lines()
+        .filter_map(|line| {
+            let mut parts = line.split("   ");
+            match (
+                parts
+                    .next()
+                    .map(str::trim)
+                    .and_then(|s| s.parse::<u32>().ok()),
+                parts
+                    .next()
+                    .map(str::trim)
+                    .and_then(|s| s.parse::<u32>().ok()),
+            ) {
+                (Some(left), Some(right)) => {
+                    right_list.entry(right).and_modify(|c| *c += 1).or_insert(1);
+                    Some(left)
+                }
+                _ => None, // Skip lines that don't have exactly two parts
+            }
+        })
+        .collect();
+    let score = left_list
+        .iter()
+        .map(|x| x * right_list.get(x).unwrap_or(&0))
+        .sum();
+    Some(score)
 }
 
 #[cfg(test)]
@@ -48,6 +77,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }

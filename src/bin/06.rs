9686034a92f9aafbc373d52bf8729c7a3e611630
Vivek Kalyan <hello@vivekkalyan.com>
2024-12-07 11:34:01 +0800

    Solve day 06 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -3,16 +3,19 @@ use std::collections::HashSet;
 use std::hash::Hash;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut map = Map::new(input);
-    map.walk(Direction{dx: 0, dy: -1});
+    let mut map = Map::new(input, Direction { dx: 0, dy: -1 });
+    map.walk(false);
     Some(map.visited.len() as u32)
 }
 
 struct Map {
     current_position: Position,
+    current_direction: Direction,
     bounds: (i32, i32),
     obstacles: Vec<Position>,
     visited: HashSet<Position>,
+    taken_path: HashSet<(Position, Direction)>,
+    obstacles_that_cause_loop: HashSet<Position>,
 }
 
 #[derive(PartialEq, Eq, Clone, Debug, Hash)]
@@ -27,20 +30,20 @@ impl Position {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 struct Direction {
     dx: i32,
     dy: i32,
 }
 
 enum WalkResult {
-    Okay,
+    Okay(Position),
     OutOfBounds,
-    Obstacle
+    Obstacle,
 }
 
 impl Map {
-    fn new(input: &str) -> Self {
+    fn new(input: &str, direction: Direction) -> Self {
         let lines: Vec<&str> = input.lines().collect();
         let height = lines.len() as i32;
         let width = lines[0].len() as i32;
@@ -67,38 +70,65 @@ impl Map {
             .expect("Starting position '^' not found");
         let mut visited = HashSet::new();
         visited.insert(start_position.clone());
+        let mut taken_path = HashSet::new();
+        taken_path.insert((start_position.clone(), direction.clone()));
 
         Map {
             current_position: start_position,
+            current_direction: direction,
             bounds: (width, height),
             obstacles,
             visited,
+            taken_path,
+            obstacles_that_cause_loop: HashSet::new(),
         }
     }
 
-    fn walk_result(&mut self, direction: &Direction) -> WalkResult {
-        let new_position = self.current_position.one_step(direction);
-        if self.obstacles.contains(&new_position) {
-            return WalkResult::Obstacle
+    fn walk_result(&self, position: &Position, direction: &Direction) -> WalkResult {
+        let next_position = position.one_step(direction);
+        if self.obstacles.contains(&next_position) {
+            return WalkResult::Obstacle;
         }
-        if new_position.x >= self.bounds.0 || new_position.y >= self.bounds.1 || new_position.x < 0 || new_position.y < 0 {
-            return WalkResult::OutOfBounds
+        if !self.is_within_bounds(&next_position) {
+            return WalkResult::OutOfBounds;
         }
-        self.current_position = new_position;
-        WalkResult::Okay
+        WalkResult::Okay(next_position)
     }
 
-    fn walk(&mut self, direction: Direction) {
-        let mut current_direction = direction;
+    fn is_within_bounds(&self, position: &Position) -> bool {
+        position.x >= 0
+            && position.y >= 0
+            && position.x < self.bounds.0
+            && position.y < self.bounds.1
+    }
+
+    fn walk(&mut self, check_hypothetical_loops: bool) {
         loop {
-            match self.walk_result(&current_direction) {
-                WalkResult::Okay => {
+            match self.walk_result(&self.current_position, &self.current_direction) {
+                WalkResult::Okay(next_position) => {
+                    // potential optimization: only check loops which hit an obstacles
+                    // potential optimization: get full taken_path first, then run this in parallel
+                    if check_hypothetical_loops && (!self.visited.contains(&next_position)) {
+                        self.obstacles.push(next_position.clone());
+                        if self.does_hypothetical_walk_create_loop(
+                            &self.current_position,
+                            &self.current_direction,
+                        ) {
+                            self.obstacles_that_cause_loop.insert(next_position.clone());
+                        }
+                        self.obstacles.pop();
+                    }
+                    self.current_position = next_position;
                     self.visited.insert(self.current_position.clone());
+                    self.taken_path.insert((
+                        self.current_position.clone(),
+                        self.current_direction.clone(),
+                    ));
                 }
                 WalkResult::Obstacle => {
-                    current_direction = Direction {
-                        dx: -current_direction.dy,
-                        dy: current_direction.dx
+                    self.current_direction = Direction {
+                        dx: -self.current_direction.dy,
+                        dy: self.current_direction.dx,
                     };
                 }
                 WalkResult::OutOfBounds => {
@@ -107,10 +137,45 @@ impl Map {
             }
         }
     }
+
+    fn does_hypothetical_walk_create_loop(
+        &self,
+        position: &Position,
+        direction: &Direction,
+    ) -> bool {
+        let mut new_position = position.clone();
+        let mut new_direction = Direction {
+            dx: -direction.dy,
+            dy: direction.dx,
+        };
+        let mut new_taken_path = self.taken_path.clone();
+        loop {
+            new_taken_path.insert((new_position.clone(), new_direction.clone()));
+            match self.walk_result(&new_position, &new_direction) {
+                WalkResult::Okay(next_position) => {
+                    new_position = next_position;
+                }
+                WalkResult::Obstacle => {
+                    new_direction = Direction {
+                        dx: -new_direction.dy,
+                        dy: new_direction.dx,
+                    };
+                    if new_taken_path.contains(&(new_position.clone(), new_direction.clone())) {
+                        return true;
+                    };
+                }
+                WalkResult::OutOfBounds => {
+                    return false;
+                }
+            }
+        }
+    }
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut map = Map::new(input, Direction { dx: 0, dy: -1 });
+    map.walk(true);
+    Some(map.obstacles_that_cause_loop.len() as u32)
 }
 
 #[cfg(test)]
@@ -126,6 +191,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
